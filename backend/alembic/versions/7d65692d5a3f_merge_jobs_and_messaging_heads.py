3fdb5af51831fd218422f1d8b2ba17556fb04a52
kanchan874 <kanchangaikwad2006@gmail.com>
2026-07-23 23:12:30 +0530

    style: format migration file with latest black to match CI

--- backend/alembic/versions/7d65692d5a3f_merge_jobs_and_messaging_heads.py
+++ backend/alembic/versions/7d65692d5a3f_merge_jobs_and_messaging_heads.py
@@ -8,7 +8,6 @@ Create Date: 2026-07-23 22:01:21.151452
 
 from typing import Sequence, Union
 
-
 # revision identifiers, used by Alembic.
 revision: str = "7d65692d5a3f"
 down_revision: Union[str, None] = ("3b13848fe022", "d4e5f6a7b8c9")
